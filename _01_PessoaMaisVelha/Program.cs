57752531a2a95187730df652f28f1ac1482aca4d
widemar <widemarinternship@gmail.com>
2022-07-05 11:23:28 -0300

    C# Exercícios

---
+++ _01_PessoaMaisVelha/Program.cs
@@ -0,0 +1,19 @@
+﻿using _01_PessoaMaisVelha.entidades;
+
+Console.WriteLine("Dados da primeira pessoa: ");
+Console.Write("Nome: ");
+string nomeUm = Console.ReadLine()!;
+Console.Write("Idade: ");
+int idadeUm = int.Parse(Console.ReadLine()!);
+Pessoa pessoaUm = new Pessoa(nomeUm, idadeUm);
+
+Console.WriteLine("Dados da segunda pessoa: ");
+Console.Write("Nome: ");
+string nomeDois = Console.ReadLine()!;
+Console.Write("Idade: ");
+int idadeDois = int.Parse(Console.ReadLine()!);
+Pessoa pessoaDois = new Pessoa(nomeDois, idadeDois);
+
+string pessoaMaisVelha = pessoaUm.Idade > pessoaDois.Idade ? pessoaUm.Nome : pessoaDois.Nome;
+
+Console.WriteLine($"Pessoa mais velha: {pessoaMaisVelha}");
---
+++ _01_PessoaMaisVelha/entidades/Pessoa.cs
@@ -0,0 +1,13 @@
+namespace _01_PessoaMaisVelha.entidades;
+
+public class Pessoa
+{
+    public string Nome { get; private set; }
+    public int Idade { get; private set; }
+
+    public Pessoa(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+}
